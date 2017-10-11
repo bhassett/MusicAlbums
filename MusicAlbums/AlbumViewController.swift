94f0f24fe03f4114f086d03790cb8df8e0904362
Brian Hassett <bhassett.73@gmail.com>
2017-10-11 15:42:03 -0400

    added code file for album view

---
+++ MusicAlbums/AlbumViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumViewController.swift
+//  MusicAlbums
+//
+//  Created by Brian Hassett on 10/6/17.
+//  Copyright © 2017 bhassett. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
